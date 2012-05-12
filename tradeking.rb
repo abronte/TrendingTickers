8091808bb153f2f8241b0858c8253d168df111d0
Adam Bronte <abronte@gmail.com>
2012-05-11 17:41:34 -0700

    support for muliptle tickers

--- tradeking.rb
+++ tradeking.rb
@@ -10,8 +10,17 @@ class TK
 	@@access_token = OAuth::AccessToken.new(@consumer, @config['access_token'], @config['access_token_secret'])
 
 	def quote(ticker)
-		resp = @@access_token.get("/v1/market/ext/quotes.json?symbols=#{ticker}", {'Accept' => 'application/json'}).body
+		ext_quotes(ticker)
+	end
 
+	def quotes(tickers)
+		ext_quotes(tickers.join(','))
+	end
+
+	private
+
+	def ext_quotes(str)
+		resp = @@access_token.get("/v1/market/ext/quotes.json?symbols=#{str}", {'Accept' => 'application/json'}).body
 		resp = JSON.parse(resp)
 
 		resp['response']['quotes']['quote']
